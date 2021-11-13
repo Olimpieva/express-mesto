b52ab0a3f2d110f3ac14d311bd6ae74607efc6c5
Olimpieva Natalya <ukurennyi.koshak@gmail.com>
2021-11-13 03:02:44 +0300

    feat: update user info/avatar

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,18 @@
 const User = require('../models/user');
 
 module.exports.getAllUsers = (req, res) => {
-  console.log(req.user);
   User.find({})
     .then((data) => res.send({ data }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка сервера' }));
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка сервера: ${err}` }));
 };
 
 module.exports.getUserById = (req, res) => {
-  User.findById(req.params.userId)
+  const userId = req.user._id;
+
+  User.findById(userId)
     .then((user) => {
       if (user) {
-        res.send({ data: user });
+        res.status(200).send({ data: user });
       } else {
         res.status(404).send({ message: 'Пользователь не найден' });
       }
@@ -20,20 +21,63 @@ module.exports.getUserById = (req, res) => {
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Переданы некорректные данные' });
       } else {
-        res.status(500).send({ message: 'Произошла ошибка' });
+        res.status(500).send({ message: `Произошла ошибка: ${err}` });
       }
     });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
+
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные' });
       } else {
-        res.status(500).send({ message: 'Произошла ошибка' });
+        res.status(500).send({ message: `Произошла ошибка: ${err}` });
+      }
+    });
+};
+
+module.exports.updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  const userId = req.user._id;
+
+  User.findByIdAndUpdate(userId, { name, about })
+    .then((user) => {
+      if (user) {
+        res.status(200).send({ data: user });
+      } else {
+        res.status(404).send({ message: 'Пользователь не найден' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(500).send({ message: `Произошла ошибка: ${err}` });
+      }
+    });
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  const userId = req.user._id;
+
+  return User.findByIdAndUpdate(userId, { avatar })
+    .then((user) => {
+      if (user) {
+        res.status(200).send(user);
+      } else {
+        res.status(404).send({ message: 'Пользователь не найден' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(500).send({ message: `Произошла ошибка: ${err}` });
       }
     });
 };
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
 const routerUsers = require('express').Router();
-const { getAllUsers, createUser, getUserById } = require('../controllers/users');
+const {
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateProfile,
+  updateAvatar,
+} = require('../controllers/users');
 
 routerUsers.get('/users', getAllUsers);
 
 routerUsers.post('/users', createUser);
 
 routerUsers.get('/users/:userId', getUserById);
+
+routerUsers.patch('/users/me', updateProfile);
+
+routerUsers.patch('/users/me/avatar', updateAvatar);
+// 618ec31488606b72b04354ae
+// 618ec39466569be6af15a61f
 
 module.exports = routerUsers;
