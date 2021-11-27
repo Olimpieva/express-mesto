0f15b30886bb2912f09d88d1963f8254e0bf9f06
Olimpieva Natalya <ukurennyi.koshak@gmail.com>
2021-11-27 14:20:54 +0300

    fix: validate routes, eslint && refactor: validate password

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi, errors } = require('celebrate');
 
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/error-determinant');
@@ -21,15 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
 
-app.get('/', (req, res) => res.send('Its working!'));
+app.get('/', (req, res) => res.send('It\'s working!'));
 
 app.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required().min(8)
-        .pattern(/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}$/),
+      password: Joi.string().required(),
       name: Joi.string().default('Жак-Ив Кусто').min(2).max(30),
       about: Joi.string().default('Исследователь').min(2).max(30),
       avatar: Joi.string()
@@ -55,9 +54,10 @@ app.use(auth);
 app.use('/', routerUsers);
 app.use('/', routerCards);
 app.use('*', (req, res, next) => {
-  next(new NotFoundError('Запрашиваемая страница не найдена'));
+  next(new NotFoundError('Запрашиваемая страница не найдена.'));
 });
 
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
